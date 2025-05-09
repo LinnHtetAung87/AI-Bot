dc9458f59076c6db4f2a63560e64afb71b986b02
Linn Htet Aung <98522300+LinnHtetAung87@users.noreply.github.com>
2025-05-09 14:39:58 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Serve static files
 // app.use(express.static(path.join(__dirname, 'build')));
-// app.get('*', (req, res) => {
+// app.get('https://ai-bot-tqg9.onrender.com', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
 
